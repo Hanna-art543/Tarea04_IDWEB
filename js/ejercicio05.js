30b7bcb60ed270f0169bd0717c62efe49fbbf771
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-22 19:18:57 -0500

    Subir ejercicio 05

---
+++ js/ejercicio05.js
@@ -0,0 +1,9 @@
+const cajas = document.querySelectorAll(".caja");
+
+cajas.forEach(caja => {
+    caja.addEventListener("click", () => {
+        cajas.forEach(c => c.classList.remove("activa"));
+
+        caja.classList.add("activa");
+    });
+});
